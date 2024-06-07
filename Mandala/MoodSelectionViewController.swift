af933c154a4a51155699489bedefcecb2ab5120b
Josh Truong <joshuatruong@outlook.com>
2024-06-07 12:52:36 -0700

    Ch. 17 Extensions and Container View Controllers complete

--- Mandala/MoodSelectionViewController.swift
+++ Mandala/MoodSelectionViewController.swift
@@ -12,6 +12,15 @@ class MoodSelectionViewController: UIViewController {
     @IBOutlet var stackView: UIStackView!
     @IBOutlet var addMoodButton: UIButton!
     
+    @IBAction func addMoodTapped(_ sender: Any) {
+        guard let currentMood = currentMood else {
+            return
+        }
+
+        let newMoodEntry = MoodEntry(mood: currentMood, timestamp: Date())
+        moodsConfigurable.add(newMoodEntry)
+    }
+    
     var moods: [Mood] = [] {
         didSet {
             currentMood = moods.first
@@ -48,6 +57,8 @@ class MoodSelectionViewController: UIViewController {
         }
     }
     
+    var moodsConfigurable: MoodsConfigurable!
+    
     @objc func moodSelectionChanged(_ sender: UIButton) {
         // Listens wo then the mood slection changes
         guard let selectedIndex = moodButtons.firstIndex(of: sender) else {
@@ -63,6 +74,21 @@ class MoodSelectionViewController: UIViewController {
         moods = [.happy, .sad, .angry, .goofy, .crying, .confused, .sleepy, .meh]
         
         addMoodButton.layer.cornerRadius = addMoodButton.bounds.height / 2
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "embedContainerViewController"?:
+            guard let moodsConfigurable = segue.destination as? MoodsConfigurable else {
+                preconditionFailure("View controller expected to conform to MoodsConfigurable")
+            }
+            self.moodsConfigurable = moodsConfigurable
+            segue.destination.additionalSafeAreaInsets =
+                                    UIEdgeInsets(top: 0, left: 0, bottom: 160, right: 0)
+            // If this happens, you need to set the "embedContainerViewController" identifier value for the segue in the storyboard
+        default:
+            preconditionFailure("Unexpected segue identifier")
+        }
     }
 
 }
---
+++ Mandala/MoodsConfigurable.swift
@@ -0,0 +1,13 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Joshua Truong on 6/7/24.
+//  Copyright © 2024 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
